0cf6c27d5354f12febe6264110fc9bd442c45aab
Leonardo Lara <leonardo@science.clinic>
2022-08-02 03:24:33 -0300

    Finished application

---
+++ src/main.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from predict import get_predictions
+import pandas as pd
+
+# Original frequencies format
+orig_col = ['500k', '1k', '2k', '3k', '4k', '6k', '8k']
+
+if __name__ == "__main__":
+    st.title("AUDIOGRAM PREDICT")
+    st.markdown("Choose your values dB (Decibels) for frequencies (2k, 4k, 6k)")
+    _2k_freq = st.number_input("2k", value=0, step=5, min_value=-5, max_value=95)
+    _4k_freq = st.number_input("4k", value=0, step=5, min_value=-5, max_value=95)
+    _6k_freq = st.number_input("6k", value=0, step=5, min_value=-5, max_value=95)
+
+    comb = ['2k','4k','6k']
+    freqs = [_2k_freq, _4k_freq, _6k_freq]
+    try:
+        freqs = list(map(float, freqs))
+    except:
+        st.write('Wrong input')
+
+    new_freq = st.checkbox('Add a new frequency?')
+    if new_freq:
+        freq_value = st.number_input("Value", value=0, step=5, min_value=-5, max_value=95)
+        freq_name = st.selectbox("Frequency:", ('500k','1k','3k','8k'))
+        comb.append(freq_name)
+        try:
+            freqs.append(float(freq_value))
+        except:
+            st.write('Wrong input')
+
+    _input = (comb, freqs)
+    if all(freqs):
+        preds = [int(i) for i in get_predictions(_input)]
+        if preds:
+            data = {}
+            for i,c in enumerate(orig_col):
+                data[c] = [preds[i]]
+            df_res = pd.DataFrame(data)
+            st.subheader('Results:')
+            st.dataframe(df_res)
+        else:
+            st.write('Wrong input')
+    
---
+++ src/predict.py
@@ -0,0 +1,66 @@
+import numpy as np
+import joblib
+import pickle
+import pandas as pd
+
+from pathlib import Path
+
+# Original frequencies format
+orig_col = ['500k', '1k', '2k', '3k', '4k', '6k', '8k']
+
+# Model Path
+m_path = Path.cwd() / 'model'
+
+# Load required files
+with open(m_path / 'models.pkl', 'rb') as f:
+    pickle_models = pickle.load(f)
+
+def get_closer_5_multi(n):
+    """
+    Get the closest 5 multiple
+    input:  number (float)
+    output: number (int)
+    """
+    r = n % 5
+    r = n+(5-r) if r>=2.5 else n-r
+    return int(r)
+
+def find_audio_model(comb):
+    """
+    Find the ML model to given frequency combination
+    input:  comb (Frequencies combination)
+    output: x (Frequencies to analyze)
+            y (Target frequency)
+            model_name (Model name)
+    """
+    for model in pickle_models:
+        x, y, model_name = [model['x'], model['y'], model['model_name']]
+        if len(comb)!=len(x):continue
+        if all((i in x) for i in comb):
+            return x, y, model_name
+    
+    return None
+
+def get_predictions(_input):
+    """
+    Get prediction from given values
+    input:  _input (Frequencies names and values)
+    output: predicted frequencies
+    """
+    comb, freqs = _input
+    comb_s = len(comb)
+    preds = []
+    for i in range(comb_s, 7):
+        model_inf = find_audio_model(comb)
+        if model_inf:
+            x, y, model_name = find_audio_model(comb)
+        else:
+            return None
+        model = joblib.load(m_path / model_name)
+        pred = get_closer_5_multi(model.predict(np.array([freqs]))[0])
+        comb += [y]
+        freqs += [pred]
+        
+    orig_idx = [orig_col.index(i) for i in comb]
+    res = [i for _,i in sorted(zip(orig_idx,freqs))]
+    return res
